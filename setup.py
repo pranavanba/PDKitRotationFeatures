1a51ec509c622b2140fe7cc85bb956cf1791c13c
Pranav Anbarasu <pranavanba@gmail.com>
2024-09-13 17:44:19 -0700

    Update dependency versions

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     author='Aryton Tediarjo and Larsson Omberg',
     author_email='aryton.tediarjo@sagebase.org',
     packages=['PDKitRotationFeatures'],
-    install_requires=["numpy",
+    install_requires=["numpy=1.20.0",
                       "pandas==1.0.3",
-                      "scipy",
+                      "scipy==1.7.3",
                       "pdkit==1.2",
-                      "scikit-learn",
-                      "tsfresh",
-                      "future",
-                      "matplotlib",
-                      "pandas_validator"]
+                      "scikit-learn==1.0.2",
+                      "tsfresh==0.19.0",
+                      "future==0.18.2",
+                      "matplotlib==3.5.1",
+                      "pandas-validator==0.5.0"]
 )
